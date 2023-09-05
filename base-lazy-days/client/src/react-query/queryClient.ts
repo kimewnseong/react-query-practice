f21bcd85df68f0887329f94c7e9d0267056168a5
kimewnseong <rladbstjd96@naver.com>
2023-09-05 15:40:18 +0900

    change useQuery global options

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -19,6 +19,11 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       onError: queryErrorHandler,
+      staleTime: 600000,
+      cacheTime: 900000, // 기본 캐시타임은 5분. staleTime이 cacheTime보다 크면 만료된 시간동안 보여줄 캐시데이터가 없으므로 옳지 않음
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
     },
   },
 });
